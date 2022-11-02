b10e6d289abc3b2780be09118e015c5aa621be00
ngawi01 <117059058+ngawi01@users.noreply.github.com>
2022-11-02 12:09:27 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.Api = {
+	ampibi: 'https://ampibismm.my.id',
+}
+
+// Apikey Website Api
+global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
+
+global.smm = {
+  nomor : '6283898869011',
+  atasnama : 'IRON-BOT'
+}
+
+// Other
+global.owner = ['6283898869011']
+global.premium = ['6283898869011']
+global.sponsor = 'no sponsors in:('
+global.packname = 'Iron-Bot'
+global.footer = '© 2022 Shwva_Tyyy'
+global.author = 'Owner : Shwva_Tyyy\nIg : @shwva_tyyy'
+global.prefa = ['','!','#','.','🐦','🐤','🗿']
+global.act = {
+		wait: 'please wait a moment, processing data...',
+		den:'*Sorry You Not Register Yet...*\n\n*.reg* <name>\n*To Regis/Login*',
+		proc:'Loading Please Wait..',
+		error:'Error :(',
+		success:'Success bang!',
+		repiar:'This Feature Maintace!',
+		botadm:'Jadiin gw admin dulu!',
+		admin:'Khusus admin woi!',
+		done:'Done!',
+		gc:'Only Group Allowed',
+		owner:'Only Owner Allowed'
+}
+
+global.thumb = fs.readFileSync('./lib/crizzy.jpg')
+global.crizzy = { url: "https://g.top4top.io/p_2443v88g61.jpg"}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
